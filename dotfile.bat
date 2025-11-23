cb282c082ff04c5e9588f98f846f9a019501c46c
Soyo <ainaba601@gmail.com>
2025-11-23 14:38:28 +0000

    Add files via upload

---
+++ dotfile.bat
@@ -0,0 +1,9 @@
+winget install Microsoft.DotNet.SDK.9
+winget install Microsoft.DotNet.SDK.10
+winget install Microsoft.VisualStudioCode --override "/verysilent /mergetasks=""addcontextmenufolders,addtopath,associatewithfiles"""
+winget install Git.Git
+winget install 7zip
+winget install GnuWin32.Wget
+winget install LibreWolf.LibreWolf
+winget install keepassxc
+winget install -e --id GitHub.GitHubDesktop
